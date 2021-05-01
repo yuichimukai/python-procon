ea7fb38eab593ea5e97fb121a46d330db583a950
向井優一 <yuichi-m@weseek.co.jp>
2021-05-01 17:36:54 +0900

    first commit

---
+++ script.py
@@ -0,0 +1,17 @@
+x = [1, 2, 3]
+print(x)
+
+for i in x:
+    print(i)
+
+y = [[1, 2, 3], [4, 5, 6]]
+print(y)
+
+for z in y:
+    for columns in z:
+        print(columns)
+
+q = [[2, 3, 4], [4, 5, 4]]
+for i in range(2):
+    for j in range(3):
+        print(q[i][j])
---
+++ script2.py
@@ -0,0 +1,21 @@
+
+nyuroku = int(input())
+print(nyuryoku)
+
+x, y, z = input()
+print(x, y, z)
+
+x, y = map(int, input().split())
+print(x)
+print(y)
+
+A, B = map(int, input().split())
+print(B, A)
+
+X, Y, Z = map(int, input().split())
+print(Z, X, Y)
+
+x = list(map(int, input().split()))
+print(x)
+
+x = [int(i) for i in input().split()]
